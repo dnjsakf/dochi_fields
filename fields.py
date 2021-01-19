1197548ad2b624c6caab9e7b1a557beda3f40da5
14D00944 <14D00944@skvdiweb.com>
2021-01-19 18:01:42 +0900

    Added other data type fields.

--- fields.py
+++ fields.py
@@ -49,4 +49,94 @@ class BaseField(object):
   
   def __str__(self):
     return str(self.__value)
- 
+
+    
+# 문자열 필드
+class StringField(BaseField):
+  def __init__(self, maxlength=None, **kwargs):
+    super(StringField, self).__init__(str, **kwargs)
+    
+    # 유효성검사 옵션값
+    self.__maxlength = maxlength
+  
+  # Overriding: 유효성검사
+  def validate(self, value=None):
+    exists = value is not None
+    value = value if exists else self.getValue()
+    length = len(value) if exists else 0
+    messages = self.getOption("messages", dict())
+    
+    # 유효성검사: 필수값
+    if bool(self.required) and not exists:
+      msg = "This value was Required, but it is None."
+      msg = messages.get("required", msg)
+      raise ValueError(msg)
+    
+    # 유효성검사: 최대길이
+    if bool(self.__maxlength) and exists and length > self.__maxlength:
+      msg = "Expacted value length %d, but %d." % ( self.__maxlength, length )
+      msg = messages.get("maxlength", msg)
+      raise ValueError(msg)
+
+# 정수형 필드
+class IntegerField(BaseField):
+  def __init__(self, min=None, max=None, **kwargs):
+    super(IntegerField, self).__init__(int, **kwargs)
+
+    # 유효성검사 옵션값
+    self.__min = min
+    self.__max = max
+  
+  # Overriding: 유효성검사
+  def validate(self, value=None):
+    exists = value is not None
+    value = value if exists else self.getValue()
+    messages = self.getOption("messages", dict())
+    
+    # 유효성검사: 필수값
+    if bool(self.required) and not exists:
+      msg = "This value was Required, but it is None."
+      msg = messages.get("required", msg)
+      raise ValueError(msg)
+    
+    # 유효성검사: 최소값
+    if bool(self.__min) and exists and value < self.__min:
+      msg = "Expacted minimum %d, but %d." % ( self.__min, value )
+      msg = messages.get("min", msg)
+      raise ValueError(msg)
+
+    # 유효성검사: 최댓값
+    if bool(self.__max) and exists and value > self.__max:
+      msg = "Expacted maximum %d, but %d." % ( self.__max, value )
+      msg = messages.get("max", msg)
+      raise ValueError(msg)
+
+      
+
+from datetime import datetime   
+
+# 날짜형 필드
+class DatetimeField(BaseField):
+  def __init__(self, format="%Y-%m-%d %H:%M:%S", *args, **kwargs):
+    super(DatetimeField, self).__init__(datetime, *args, **kwargs)
+
+    self.__value = None
+    self.__format = format
+
+  # 값 설정
+  def setValue(self, value, format=None, validate=True):
+    # 값이 문자열인 경우, datetime으로 변환
+    if isinstance(value, str):
+      value = datetime.strptime(value, format or self.__format)
+
+    self.__value = None
+    
+    if validate:
+      # 값이 있는 경우 타입체크
+      if value is not None and not isinstance(value, self.type):
+        raise TypeError("Expacted data type '%s', but '%s'." %( self.type.__name__, value.__class__.__name__ ))
+      
+      # 유효성검사
+      self.validate(value)
+    
+    self.__value = value
